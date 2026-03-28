8e980b091507cc6097e2171fdbefb7f816e24eb8
changhui98 <net_0102@naver.com>
2026-03-28 21:41:11 +0900

    Feat : 글로벌 예외 처리 기능 구현

---
+++ src/main/java/com/peopleground/moida/global/exception/ApiErrorCode.java
@@ -0,0 +1,22 @@
+package com.peopleground.moida.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApiErrorCode implements ErrorCode {
+
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "A001", "잘못된 요청입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "A002", "접근 권한이 없습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A003", "인증이 필요합니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "A999", "서버 내부 오류가 발생했습니다.")
+
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
