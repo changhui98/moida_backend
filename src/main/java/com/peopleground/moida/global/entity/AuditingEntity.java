852268f048c2da5e76f8c0a54fcc4de77fa28c2c
changhui98 <net_0102@naver.com>
2026-03-31 23:22:10 +0900

    Feat : 공용 엔티티 생성

---
+++ src/main/java/com/peopleground/moida/global/entity/AuditingEntity.java
@@ -0,0 +1,31 @@
+package com.peopleground.moida.global.entity;
+
+import com.peopleground.moida.user.domain.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@Getter
+@MappedSuperclass
+public class AuditingEntity extends BaseEntity{
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, length = 50)
+    protected String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by", length = 50)
+    protected String lastModifiedBy;
+
+    @Column(name = "deleted_by", length = 50)
+    protected String deletedBy;
+
+    public void deleteBy(User user) {
+        this.deletedDate = LocalDateTime.now();
+        this.deletedBy = user.getUsername();
+    }
+
+}
