bd140646d3973110c0ccb58716f2de67ce3f71dc
changhui98 <net_0102@naver.com>
2026-04-14 22:49:42 +0900

    Feat : 게시글 작성 기능 구현

--- src/main/java/com/peopleground/moida/content/domain/entity/Content.java
+++ src/main/java/com/peopleground/moida/content/domain/entity/Content.java
@@ -1,5 +1,51 @@
 package com.peopleground.moida.content.domain.entity;
 
-public class Content {
+import com.peopleground.moida.global.entity.AuditingEntity;
+import com.peopleground.moida.user.domain.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity(name = "p_content")
+public class Content extends AuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(max = 20)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public static Content of(
+        String title,
+        String body,
+        User user
+    ) {
+        Content content = new Content();
+        content.title = title;
+        content.body = body;
+        content.user = user;
+        return content;
+    }
 }
--- src/main/java/com/peopleground/moida/content/infrastructure/repository/ContentRepositoryImpl.java
+++ src/main/java/com/peopleground/moida/content/infrastructure/repository/ContentRepositoryImpl.java
@@ -1,9 +1,19 @@
 package com.peopleground.moida.content.infrastructure.repository;
 
+import com.peopleground.moida.content.domain.entity.Content;
 import com.peopleground.moida.content.domain.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class ContentRepositoryImpl implements ContentRepository {
 
+    private final ContentJpaRepository contentJpaRepository;
+
+    @Override
+    public Content save(Content content) {
+
+        return contentJpaRepository.save(content);
+    }
 }
